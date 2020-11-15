72b7965eb5c70c9aeec37ccb4946e13514ada25f
Andrew Prokopenko <i.andrewprokopenko@gmail.com>
2020-11-15 16:04:09 +0300

    Updates

---
+++ precache-manifest.4e7ec448705c953e1a92da8a65438fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e96ae69faaea4302bb134a2ab0e3f7c",
+    "url": "/drop1/index.html"
+  },
+  {
+    "revision": "edc1078c44c3ccc2100c",
+    "url": "/drop1/static/css/main.987a1c7a.chunk.css"
+  },
+  {
+    "revision": "819d74272306f015bc9f",
+    "url": "/drop1/static/js/2.d01960ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drop1/static/js/2.d01960ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edc1078c44c3ccc2100c",
+    "url": "/drop1/static/js/main.d6b929f2.chunk.js"
+  },
+  {
+    "revision": "763b26d4c532725596fe",
+    "url": "/drop1/static/js/runtime-main.75596311.js"
+  }
+]);
